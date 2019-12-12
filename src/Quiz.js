86b9e696f97f455421dfc6dec6caf41e006875a4
AACO\ITNEIS00 <itneis00@aacounty.org>
2019-12-12 13:48:36 -0500

    Resetting the Quiz

--- src/Quiz.js
+++ src/Quiz.js
@@ -14,6 +14,9 @@ class Quiz extends Component {
       return { quiz_position: state.quiz_position + 1 };
     });
   }
+  handleResetClick() {
+    this.setState({ quiz_position: 1 });
+  }
 
   render() {
     const isQuizEnd =
@@ -21,7 +24,7 @@ class Quiz extends Component {
     return (
       <div>
         {isQuizEnd ? (
-          <QuizEnd />
+          <QuizEnd resetClickHandler={this.handleResetClick.bind(this)} />
         ) : (
           <QuizQuestion
             quiz_question={
